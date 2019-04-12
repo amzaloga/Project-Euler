06a56f3924407f671f661186ebb105272ee73ea6
amzaloga <30347270+amzaloga@users.noreply.github.com>
2019-04-11 21:34:41 -0400

    Update Fibonacci.java

--- Fibonacci.java
+++ Fibonacci.java
@@ -1,6 +1,6 @@
 //****************************************************************
 //This returns the sum of the series of fibbonacci's sequence. 
-//The 32nd occurance happens to be under 4,000,000
+//The 32nd occurance happens to be under 4,000,000 ALeks
 //*****************************************************************
 public class Fibonacci {
 	public static void main(String[] args) {
